8b240d6231e72a73131e7b787fc87148408458b0
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-12-05 22:46:56 +0300

    feat(IService) : created

---
+++ RestaurantApp.Application/Interfaces/Services/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using RestaurantApp.Application.DTOs.Products;
+
+namespace RestaurantApp.Application.Interfaces.Services
+{
+    public interface IProductService
+    {
+        Task<List<ResultProductDto>> GetAllAsync();
+        Task<List<ResultProductDto>> GetDailySpecialsAsync();
+        Task<GetProductByIdDto> GetByIdAsync(int id);
+        Task AddAsync(CreateProductDto createProductDto);
+        Task UpdateAsync(UpdateProductDto updateProductDto);
+        Task DeleteAsync(int id);
+    }
+}

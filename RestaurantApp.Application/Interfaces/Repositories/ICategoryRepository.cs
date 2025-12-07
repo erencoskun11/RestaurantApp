a734614e9a4f816dc86af39eb2005ae40d904a40
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-12-07 11:55:09 +0300

    feat(IRepository) : created

---
+++ RestaurantApp.Application/Interfaces/Repositories/ICategoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using RestaurantApp.Domain.Entities;
+
+namespace RestaurantApp.Application.Interfaces.Repositories
+{
+    public interface ICategoryRepository : IGenericRepository<Category>
+    {
+        // Kategoriye özel ekstra bir metod gerekirse buraya yazarız.
+        // Şimdilik Generic yapı yeterli.
+    }
+}

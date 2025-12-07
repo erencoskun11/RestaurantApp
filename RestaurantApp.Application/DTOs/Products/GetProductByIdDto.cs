29106183aa269c6d7e562d433555a42e001712d8
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-12-07 11:52:35 +0300

    feat(dto) : created

---
+++ RestaurantApp.Application/DTOs/Products/GetProductByIdDto.cs
@@ -0,0 +1,15 @@
+﻿namespace RestaurantApp.Application.DTOs.Products
+{
+    public class GetProductByIdDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+        public int CategoryId { get; set; } // Güncelleme ekranında ID lazım olabilir
+        public string CategoryName { get; set; }
+        public bool IsDailySpecial { get; set; }
+        public bool IsChefSuggestion { get; set; }
+    }
+}

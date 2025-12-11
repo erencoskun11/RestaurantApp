a0d28adf492d93e64afe20bf0270b834178b2018
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-12-11 23:09:14 +0300

    feat(IRepository) : created

---
+++ RestaurantApp.Application/Interfaces/Repositories/IProductRepository.cs
@@ -0,0 +1,9 @@
+﻿using RestaurantApp.Domain.Entities;
+
+namespace RestaurantApp.Application.Interfaces.Repositories
+{
+    public interface IProductRepository : IGenericRepository<Product>
+    {
+        Task<List<Product>> GetDailySpecialsAsync();
+    }
+}

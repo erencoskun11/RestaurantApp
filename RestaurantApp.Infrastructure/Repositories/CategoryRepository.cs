233848d23fdb4595012ba72a8d58ceeba5073a24
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-12-11 23:20:17 +0300

    feat(repository) : created

---
+++ RestaurantApp.Infrastructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using RestaurantApp.Application.Interfaces.Repositories;
+using RestaurantApp.Domain.Entities;
+using RestaurantApp.Infrastructure.Persistence;
+
+namespace RestaurantApp.Infrastructure.Repositories
+{
+    public class CategoryRepository : GenericRepository<Category>, ICategoryRepository
+    {
+        public CategoryRepository(RestaurantContext context) : base(context)
+        {
+        }
+    }
+}
